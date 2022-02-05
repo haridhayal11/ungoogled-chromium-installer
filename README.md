678003a22b3171b747abb7b75b8f3b8d36c441b4
haridhayal11 <haridhayal@gmail.com>
2022-02-05 16:31:48 +0530

    Add instructions and improve the script
    Signed-off-by: haridhayal11 <haridhayal@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ungoogled-chromium-installer
+
+1. Download Ungoogled-Chromium portable tar from https://github.com/macchrome/linchrome/releases
+2. Run the commands below within the same directory of Downloaded *.tar.xz file
+
+```
+wget https://raw.githubusercontent.com/haridhayal11/ungoogled-chromium-installer/main/ungoogled-chromium-installer.sh && chmod +x ungoogled-chromium-installer.sh
+bash ungoogled-chromium-installer.sh
+```
--- ungoogled-chromium-installer.sh
+++ ungoogled-chromium-installer.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-#Ungoogled-Chromium Portable Install script
+# Ungoogled-Chromium Portable Install script
 
 # Clean Old versions
 sudo rm -rf /opt/ungoogled-chromium_*/
 sudo rm -rf /opt/ungoogled-chromium
 
-# Copy New version
+# Extract New version
 sudo tar -xvf ungoogled-chromium_*.tar.xz -C /opt
+
+# Create Shortcut
+wget https://raw.githubusercontent.com/haridhayal11/ungoogled-chromium-installer/main/ungoogled-chromium.desktop
 sudo cp -r ungoogled-chromium.desktop /usr/share/applications/
 sudo chown root:root /usr/share/applications/ungoogled-chromium.desktop
 sudo chmod 644 /usr/share/applications/ungoogled-chromium.desktop
+
+# Set Permissions
 sudo chown -R root:root /opt/ungoogled-chromium_*/
 sudo mv -vuf /opt/ungoogled-chromium_*/chrome_sandbox /opt/ungoogled-chromium_*/chrome-sandbox
 sudo chmod 4775 chrome-sandbox
