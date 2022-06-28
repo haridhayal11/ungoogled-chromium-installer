38e0d7ec0e7650ad6004359d41960ec3eb1ad006
haridhayal11 <haridhayal@gmail.com>
2022-06-28 07:23:13 +0530

    Improve script

--- ungoogled-chromium-installer.sh
+++ ungoogled-chromium-installer.sh
@@ -9,6 +9,7 @@ sudo rm -rf /opt/ungoogled-chromium
 sudo tar -xvf ungoogled-chromium_*.tar.xz -C /opt
 
 # Create Shortcut
+rm -rf ungoogled-chromium.desktop
 wget https://raw.githubusercontent.com/haridhayal11/ungoogled-chromium-installer/main/ungoogled-chromium.desktop
 sudo cp -r ungoogled-chromium.desktop /usr/share/applications/
 sudo chown root:root /usr/share/applications/ungoogled-chromium.desktop
@@ -16,6 +17,8 @@ sudo chmod 644 /usr/share/applications/ungoogled-chromium.desktop
 
 # Set Permissions
 sudo chown -R root:root /opt/ungoogled-chromium_*/
-sudo mv -vuf /opt/ungoogled-chromium_*/chrome_sandbox /opt/ungoogled-chromium_*/chrome-sandbox
+cd /opt/ungoogled-chromium_*/
+sudo mv -vuf chrome_sandbox chrome-sandbox
 sudo chmod 4775 chrome-sandbox
+cd /opt/
 sudo ln -s /opt/ungoogled-chromium_*/chrome-wrapper /opt/ungoogled-chromium
